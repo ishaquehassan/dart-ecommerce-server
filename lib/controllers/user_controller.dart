0ac9423bd985684058232a4f6ebd29c7cf3ef239
Ishaq Hassan <ishaquehassan@gmail.com>
2022-04-14 06:13:10 +0500

    Added separate file for GUIDGen

--- lib/controllers/user_controller.dart
+++ lib/controllers/user_controller.dart
@@ -1,4 +1,4 @@
-import 'dart:math' as Math;
+
 
 import 'package:alfred_test_project/base/response_list_model.dart';
 import 'package:alfred_test_project/base/response_model.dart';
@@ -6,32 +6,9 @@ import 'package:alfred_test_project/models/user.dart';
 
 import '../base/api_controller.dart';
 import '../base/base_response_model.dart';
+import '../base/guid_gen.dart';
 
-class GUIDGen {
 
-  static String generate() {
-    Math.Random random = new Math.Random(new DateTime.now().millisecond);
-
-    final String hexDigits = "0123456789abcdef";
-    final List<String> uuid = List.generate(36, (index) => "");
-
-    for (int i = 0; i < 36; i++) {
-      final int hexPos = random.nextInt(16);
-      uuid[i] = (hexDigits.substring(hexPos, hexPos + 1));
-    }
-
-    int pos = (int.parse(uuid[19], radix: 16) & 0x3) | 0x8; // bits 6-7 of the clock_seq_hi_and_reserved to 01
-
-    uuid[14] = "4";  // bits 12-15 of the time_hi_and_version field to 0010
-    uuid[19] = hexDigits.substring(pos, pos + 1);
-
-    uuid[8] = uuid[13] = uuid[18] = uuid[23] = "-";
-
-    final StringBuffer buffer = new StringBuffer();
-    buffer.writeAll(uuid);
-    return buffer.toString();
-  }
-}
 
 class UserController extends ApiController{
   @override
