94171e4d2e37ce2ca45f62116717e4e42aabe7f8
Ishaq Hassan <ishaquehassan@gmail.com>
2022-04-15 07:37:19 +0500

    Fixed ability to extract color from category icon image URL

--- color_extract.php
+++ color_extract.php
@@ -4,47 +4,12 @@ $url=$_SERVER['argv'][1];
 if(empty($url)){
     die("Please add url param to GET request!");
 }
-$imageData = getColorPallet($url);
-foreach ($imageData as $clr){
-    if(strlen("$clr") == 6){
-        die("#$clr");
+include_once("colors.inc.php");
+$ex=new GetMostCommonColors();
+file_put_contents("temp.png",file_get_contents($url));
+$colors=$ex->Get_Color("temp.png");
+foreach ($colors as $color => $amount){
+    if("$color" != "000000"){
+        die("#$color");
     }
 }
-function getColorPallet($imageURL, $palletSize=[16,8]){ // GET PALLET FROM IMAGE PLAY WITH INPUT PALLET SIZE
-    // SIMPLE CHECK INPUT VALUES
-    if(!$imageURL) return false;
-
-    // IN THIS EXEMPLE WE CREATE PALLET FROM JPG IMAGE
-    $img = null;
-    $urlPars = explode(".",$imageURL);
-    $extension = strtolower($urlPars[count($urlPars)-1]);
-    if($extension == "png"){
-        $img = imagecreatefrompng($imageURL);
-    }elseif($extension == "jpg" || $extension == "jpeg"){
-        $img = imagecreatefromjpeg($imageURL);
-    }elseif($extension == "webp"){
-        $img = imagecreatefromwebp($imageURL);
-    }
-
-    // SCALE DOWN IMAGE
-    $imgSizes=getimagesize($imageURL);
-
-    $resizedImg=imagecreatetruecolor($palletSize[0],$palletSize[1]);
-
-    imagecopyresized($resizedImg, $img , 0, 0 , 0, 0, $palletSize[0], $palletSize[1], $imgSizes[0], $imgSizes[1]);
-
-    imagedestroy($img);
-
-    //GET COLORS IN ARRAY
-    $colors=[];
-
-    for($i=0;$i<$palletSize[1];$i++)
-        for($j=0;$j<$palletSize[0];$j++)
-            $colors[]=dechex(imagecolorat($resizedImg,$j,$i));
-
-    imagedestroy($resizedImg);
-
-    return array_unique($colors);
-
-}
-?>
